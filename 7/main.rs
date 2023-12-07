88bc4eb5701b1afb4fbfe09de7299e1ac5eddb03
Lucas Emmes <lucas_florian.emmes@asn.com>
2023-12-07 09:25:10 +0100

    Day 7 finished

--- 7/main.rs
+++ 7/main.rs
@@ -14,19 +14,19 @@ enum HandType {
 
 fn main() {
     let mut evaluate: HashMap<char, i64> = HashMap::new();
-    evaluate.insert('A', 12);
-    evaluate.insert('K', 11);
-    evaluate.insert('Q', 10);
-    evaluate.insert('J', 9);
-    evaluate.insert('T', 8);
-    evaluate.insert('9', 7);
-    evaluate.insert('8', 6);
-    evaluate.insert('7', 5);
-    evaluate.insert('6', 4);
-    evaluate.insert('5', 3);
-    evaluate.insert('4', 2);
-    evaluate.insert('3', 1);
-    evaluate.insert('2', 0);
+    evaluate.insert('A', 13);
+    evaluate.insert('K', 12);
+    evaluate.insert('Q', 11);
+    evaluate.insert('J', 10);
+    evaluate.insert('T', 9);
+    evaluate.insert('9', 8);
+    evaluate.insert('8', 7);
+    evaluate.insert('7', 6);
+    evaluate.insert('6', 5);
+    evaluate.insert('5', 4);
+    evaluate.insert('4', 3);
+    evaluate.insert('3', 2);
+    evaluate.insert('2', 1);
 
     let mut hand_evaluate: HashMap<HandType, i8> = HashMap::new();
     hand_evaluate.insert(HandType::FiveOfAKind, 6);
@@ -45,47 +45,91 @@ fn main() {
         let cards = line.split(" ").collect::<Vec<&str>>()[0];
         let bid = line.split(" ").collect::<Vec<&str>>()[1].parse::<i64>().unwrap();
         let hand = (cards, bid);
-        get_hand_type(&hand, &evaluate);
+        get_hand_type_p1(&hand, &evaluate);
         hands.push(hand);
     }
 
-    hands.sort_by(|a, b| custom_comp(a, b, &evaluate, &hand_evaluate)); // Sorts ascending
+    hands.sort_by(|a, b| custom_comp(a, b, &evaluate, &hand_evaluate, &"p1")); // Sorts ascending
 
     let mut p1 = 0;
     for i in 0..hands.len() {
         p1 += hands[i].1 * (i as i64 +1);
     }
-
     println!("P1 {}", p1);
+    
+    evaluate.insert('J', 0);
+    hands.sort_by(|a, b| custom_comp(a, b, &evaluate, &hand_evaluate, &"p2")); // Sorts ascending
+    
+    let mut p2 = 0;
+    for i in 0..hands.len() {
+        p2 += hands[i].1 * (i as i64 +1);
+    }
+    println!("P2 {}", p2);
+    
 }
 
-fn get_hand_type(hand: &(&str, i64), evaluate: &HashMap<char, i64>) -> HandType {
-    let mut values: [i8; 13] = [0; 13];
+fn get_hand_type_p1(hand: &(&str, i64), evaluate: &HashMap<char, i64>) -> HandType {
+    let mut values: [i8; 14] = [0; 14];
     for c in hand.0.chars() {
         values[*evaluate.get(&c).unwrap() as usize] += 1;
     }
     values.sort();
 
     return match values {
-        [0,0,0,0,0,0,0,0,0,0,0,0,5] => HandType::FiveOfAKind,
-        [0,0,0,0,0,0,0,0,0,0,0,1,4] => HandType::FourOfAKind,
-        [0,0,0,0,0,0,0,0,0,0,0,2,3] => HandType::FullHouse,
-        [0,0,0,0,0,0,0,0,0,0,1,1,3] => HandType::ThreeOfAKind,
-        [0,0,0,0,0,0,0,0,0,0,1,2,2] => HandType::TwoPair,
-        [0,0,0,0,0,0,0,0,0,1,1,1,2] => HandType::OnePair,
-        [0,0,0,0,0,0,0,0,1,1,1,1,1] => HandType::HighCard,
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,5] => HandType::FiveOfAKind,
+        [0,0,0,0,0,0,0,0,0,0,0,0,1,4] => HandType::FourOfAKind,
+        [0,0,0,0,0,0,0,0,0,0,0,0,2,3] => HandType::FullHouse,
+        [0,0,0,0,0,0,0,0,0,0,0,1,1,3] => HandType::ThreeOfAKind,
+        [0,0,0,0,0,0,0,0,0,0,0,1,2,2] => HandType::TwoPair,
+        [0,0,0,0,0,0,0,0,0,0,1,1,1,2] => HandType::OnePair,
+        [0,0,0,0,0,0,0,0,0,1,1,1,1,1] => HandType::HighCard,
         _ => HandType::HighCard
     }
 }
 
-fn custom_comp(a: &(&str, i64), b: &(&str, i64), evaluate: &HashMap<char, i64>, hand_evaluate: &HashMap<HandType, i8>) -> Ordering {
+fn get_hand_type_p2(hand: &(&str, i64), evaluate: &HashMap<char, i64>) -> HandType {
+    let mut values: [i8; 14] = [0; 14];
+    let mut jokers: i8 = 0;
+    for c in hand.0.chars() {
+        if c == 'J' {jokers+=1;}
+        else {
+            values[*evaluate.get(&c).unwrap() as usize] += 1;
+        }
+    }
+    values.sort();
+    values[13] += jokers;
 
-    if hand_evaluate.get(&get_hand_type(a, evaluate)) > hand_evaluate.get(&get_hand_type(b, evaluate)) {
-        return Ordering::Greater;
+    return match values {
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,5] => HandType::FiveOfAKind,
+        [0,0,0,0,0,0,0,0,0,0,0,0,1,4] => HandType::FourOfAKind,
+        [0,0,0,0,0,0,0,0,0,0,0,0,2,3] => HandType::FullHouse,
+        [0,0,0,0,0,0,0,0,0,0,0,1,1,3] => HandType::ThreeOfAKind,
+        [0,0,0,0,0,0,0,0,0,0,0,1,2,2] => HandType::TwoPair,
+        [0,0,0,0,0,0,0,0,0,0,1,1,1,2] => HandType::OnePair,
+        [0,0,0,0,0,0,0,0,0,1,1,1,1,1] => HandType::HighCard,
+        _ => HandType::HighCard
     }
-    else if hand_evaluate.get(&get_hand_type(a, evaluate)) < hand_evaluate.get(&get_hand_type(b, evaluate)) {
-        return Ordering::Less;
+}
+
+fn custom_comp(a: &(&str, i64), b: &(&str, i64), evaluate: &HashMap<char, i64>, hand_evaluate: &HashMap<HandType, i8>, ctype: &str) -> Ordering {
+
+    if ctype == "p1" {
+        if hand_evaluate.get(&get_hand_type_p1(a, evaluate)) > hand_evaluate.get(&get_hand_type_p1(b, evaluate)) {
+            return Ordering::Greater;
+        }
+        else if hand_evaluate.get(&get_hand_type_p1(a, evaluate)) < hand_evaluate.get(&get_hand_type_p1(b, evaluate)) {
+            return Ordering::Less;
+        }
     }
+    else if ctype == "p2" {
+        if hand_evaluate.get(&get_hand_type_p2(a, evaluate)) > hand_evaluate.get(&get_hand_type_p2(b, evaluate)) {
+            return Ordering::Greater;
+        }
+        else if hand_evaluate.get(&get_hand_type_p2(a, evaluate)) < hand_evaluate.get(&get_hand_type_p2(b, evaluate)) {
+            return Ordering::Less;
+        }
+    }
+    
 
     for i in 0..5 {
         if evaluate.get(&a.0.chars().collect::<Vec<char>>()[i as usize]) > evaluate.get(&b.0.chars().collect::<Vec<char>>()[i as usize]) {
