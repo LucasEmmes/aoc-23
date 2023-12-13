5a0f3d830f2362635a2f297cf573dfa70820bc5f
Lucas Emmes <lucas_florian.emmes@asn.com>
2023-12-13 16:17:16 +0100

    syn

--- 13/main.py
+++ 13/main.py
@@ -1,49 +1,97 @@
-def overlap_hor(a, b):
-    x = min(len(a), len(b))
-    for i in range(0,x):
-        if a[i] != b[i]: return False
-    return True
+from typing import List, Dict, Tuple
 
-def overlap_vert(a, b):
-    x = min(len(a), len(b))
-    for i in range(0,x):
-        if not overlap_hor(a[i], b[i]): return False
-    return True
+def get_diffs(a: List[List[str]], b: List[List[str]]) -> bool:
+    diffs = 0
+    m = min(len(a), len(b))
+    for i in range(0, m):
+        for j in range(0, len(a[i])):
+            if a[i][j] != b[i][j]: diffs += 1
+    return diffs
 
 def main():
     with open("input.txt", "r") as f:
-        sections = [i.split("\n") for i in f.read().split("\n\n")]
+        sections: List[str] = f.read().split("\n\n")
 
-    p1 = 0
-    for section in sections:
-        hori = set([i for i in range(1,len(section[0])-1)])
-        # check horizon
-        for l, line in enumerate(section):
-            hortemp = set()
-            for i in range(1, line.__len__()):
-                left = line[i-1::-1]
-                right = line[i:]
-                if overlap_hor(left, right):
-                    hortemp.add(i)
-            hori = hori.intersection(hortemp)
-        if len(hori) > 1:
-            print(f"FAILURE, HORI IS {hori}")
-            exit()
-        elif len(hori) == 1:
-            p1 += hori.pop()
-            continue
-
-        # hor
-        for i in range(1, section.__len__()-1):
-            top = section[i-1::-1]
-            bottom = section[i:]
-            if overlap_vert(top, bottom):
-                p1 += i*100
-                continue
+    # Parsing
+    sections_p1: List[List[List[str]]] = []
+    sections_p2: List[List[List[str]]] = []
+    for (sec_num, section) in enumerate(sections):
+        lines = list(filter(lambda x: len(x) > 0, section.split("\n")))
         
-        print("FAILURE HELP NOTHING FOUND")
+        temp_p1_full: List[List[str]] = []
+        temp_p2_full: List[List[str]] = []
+        for _ in range(0, lines[0].__len__()):
+            temp_p2_full.append([])
+
+        for y in range(0, lines.__len__()):
+            temp_p1_row = []
+            for x in range(0, lines[0].__len__()):
+                temp_p1_row.append(lines[y][x])
+                temp_p2_full[x].insert(0, lines[y][x])
+            temp_p1_full.append(temp_p1_row)
+
+        sections_p1.append(temp_p1_full)
+        sections_p2.append(temp_p2_full)
+
+    # Checking
+    p1 = 0
+    
+    for (section_num, (section_hor, section_ver)) in enumerate(zip(sections_p1, sections_p2)):
+        found = False
+        # Check horizontal
+        for i in range(1, len(section_hor)):
+            top = section_hor[i-1::-1]
+            bottom = section_hor[i::]
+            if get_diffs(top, bottom) == 0:
+                p1 += i*100
+                found = True
+                break
+        
+        if found: continue
+
+        # Check vertical
+        for i in range(1, len(section_ver)):
+            top = section_ver[i-1::-1]
+            bottom = section_ver[i::]
+            if get_diffs(top, bottom) == 0:
+                p1 += i
+                found = True
+                break
+        
+        if not found: print(f"Failed section {section_num}")
 
     print(f"P1 {p1}")
+
+
+    p2 = 0
+
+    for (section_num, (section_hor, section_ver)) in enumerate(zip(sections_p1, sections_p2)):
+        found = False
+        # Check horizontal
+        for i in range(1, len(section_hor)):
+            top = section_hor[i-1::-1]
+            bottom = section_hor[i::]
+            if get_diffs(top, bottom) == 1:
+                p2 += i*100
+                found = True
+                break
+        
+        if found: continue
+
+        # Check vertical
+        for i in range(1, len(section_ver)):
+            top = section_ver[i-1::-1]
+            bottom = section_ver[i::]
+            if get_diffs(top, bottom) == 1:
+                p2 += i
+                found = True
+                break
+        
+        if not found: print(f"Failed section {section_num}")
+
+    print(f"P2 {p2}")
+
+
 
 
 main()
--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,107 +1,65 @@
-use core::num;
-
 fn main() {
     let input: String = std::fs::read_to_string("demo.txt").expect("");
     let lines: Vec<&str> = input.split("\n").filter(|x| !x.is_empty()).collect::<Vec<&str>>();
 
-    let mut p1 = 0;
-    let mut p2 = 0;
+    // let mut p1 = 0;
+    // let mut p2 = 0;
     for (i, line) in lines.clone().into_iter().enumerate() {
         // Common
         let data = line.split(" ").collect::<Vec<&str>>();
         let segment_str = data[0];
         let values_str = data[1];
 
-        // P1
-        // let values = values_str.split(",").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
-        // let total_chars: i64 = segment_str.len() as i64;
-        // let total_values = values.clone().into_iter().reduce(|a, b| a+b).unwrap();
-        // let number_of_periods = total_chars - total_values;
-        
-        // let perms = create_perms(number_of_periods, values.len() as i64 + 1, 0);
-        // for perm in perms {
-        //     let masked = &apply_mask(&values, &perm)[..];
-        //     if check_match(segment_str, masked) {
-        //         p1 += 1;
-        //     }
-        // }
+        // P1 setup
+        let values = values_str.split(",").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        let total_chars: i64 = segment_str.len() as i64;
+        let total_values = values.clone().into_iter().reduce(|a, b| a+b).unwrap() + values.len() as i64 - 1;
+        let number_of_periods = total_chars - total_values;
+        // P1 check
+        let perms = create_perms(number_of_periods, values.len() as i64 + 1, 0);
+        for perm in perms {
+            let masked = apply_mask(&values, &perm);
+            if check_match(segment_str, &masked) {
+                // p1 += 1;
+            }
+        }
 
-        // P2
+        // P2 setup
         let segment_str_p2 = segment_str;
         let values_str_p2  = values_str;
-        // let mut segment_str_p2 = String::new();
-        // let mut values_str_p2  = String::new();
-        // for _ in 0..4 {
-        //     segment_str_p2.push_str(&segment_str);
-        //     segment_str_p2.push('?');
-        //     values_str_p2.push_str(&values_str);
-        //     values_str_p2.push(',');
-        // }
-        // segment_str_p2.push_str(&segment_str);
-        // values_str_p2.push_str(&values_str);
+        let mut segment_str_p2 = String::new();
+        let mut values_str_p2  = String::new();
+        for _ in 0..4 {
+            segment_str_p2.push_str(&segment_str);
+            segment_str_p2.push('?');
+            values_str_p2.push_str(&values_str);
+            values_str_p2.push(',');
+        }
+        segment_str_p2.push_str(&segment_str);
+        values_str_p2.push_str(&values_str);
         
         let values = values_str_p2.split(",").map(|x| x.parse::<i64>().unwrap()).collect::<Vec<i64>>();
         let total_chars: i64 = segment_str_p2.len() as i64;
-        let total_values = values.clone().into_iter().reduce(|a, b| a+b).unwrap();
+        let total_values = values.clone().into_iter().reduce(|a, b| a+b).unwrap() + values.len() as i64 - 1;
         let number_of_periods = total_chars - total_values;
-        
-        p2 += get_matches(number_of_periods, values_str_p2, segment_str_p2, false);
+
+        // P2 check
+        println!("{} possible perms", get_perm_count(number_of_periods, values.len() as i64+1));
+        println!("{} possible board", 2i64.pow(segment_str_p2.matches('?').count() as u32));
+        println!();
+        // let perms = create_perms(number_of_periods, values.len() as i64 + 1, 0);
+        // for perm in perms {
+        //     let masked = apply_mask(&values, &perm);
+        //     if check_match(&segment_str_p2, &masked) {
+        //         p1 += 1;
+        //     }
+        // }
     }
 
     // println!("P1 {p1}");
-    println!("P2 {p2}");
+    // println!("P2 {p2}");
 
 }
-
-fn get_matches(periods_left: i64, values_left: &str, segments_left: &str, must_be_period: bool) -> i64 {
-    if periods_left < values_left.len() as i64 - 1 {return 0;}
-    else if periods_left < values_left.len() as i64 && must_be_period == true {return 0;}
-    else if segments_left.is_empty() {return 0;}
-
-    let mut res = 0;
-    
-    let lead_segment = **&segments_left.chars().collect::<Vec<char>>().first().unwrap();
-    if lead_segment == '.' && periods_left > 0 {
-        res += get_matches(periods_left-1, values_left, &segments_left[1..segments_left.len()], false);
-    }
-    else if lead_segment == '#' && segments_left.len() > 0 {
-        // temp = "#" * values[0]
-        let mut temp = String::new();
-        let value = values_left.split(',').collect::<Vec<&str>>().first().unwrap().parse::<i64>().unwrap() as usize;
-        for _ in 0..value {temp.push('#');}
-        
-        if must_be_period == true {return 0;}
-        // Check if OK
-        else if segments_left.starts_with(&temp) {
-            // OK
-            let x = {if values_left.contains(',') {2} else {1}};
-            res += get_matches(periods_left, &values_left[x..], &segments_left[value..], true);
-        }
-        else {
-            // NOT OK
-            return 0;
-        }
-    }
-    else {
-        // Always check for period
-        res += get_matches(periods_left-1, values_left, &segments_left[1..], false);
-        // If it can be something else, try
-        if must_be_period == false && values_left.len() > 0 {
-            // temp = "#" * values[0]
-            let mut temp = String::new();
-            let value = values_left.split(',').collect::<Vec<&str>>().first().unwrap().parse::<i64>().unwrap() as usize;
-            for _ in 0..value {temp.push('#');}
-
-            if check_match(&segments_left[0..value], &temp) {
-                let x = {if values_left.contains(',') {2} else {1}};
-                res += get_matches(periods_left, &values_left[x..], &segments_left[value..], true);
-            }
-        }
-    }
- 
-    return res;
-}
-
 
 fn check_match(segment_str: &str, verification: &str) -> bool {
     let seg_vec = segment_str.chars().collect::<Vec<char>>();
@@ -114,35 +72,67 @@ fn check_match(segment_str: &str, verification: &str) -> bool {
     return true;
 }
 
-// fn apply_mask(values: &Vec<i64>, perm: &Vec<i64>) -> String {
-//     let mut res: String = String::new();
-//     for i in 0..values.len() {
-//         for p in 0..perm[i] {
-//             res.push('.');
-//         }
-//         for j in 0..values[i] {
-//             res.push('#');
-//         }
-//     }
-//     for p in 0..*perm.last().unwrap() {
-//         res.push('.');
-//     }
-//     return res;
-// }
+fn make_segment(length: i64, period: bool, is_end: bool) -> String {
+    let mut res = String::new();
+    if period {
+        for i in 0..length {res.push('.');}
+        return res;
+    }
+    else {
+        for i in 0..length {res.push('#');}
+        if !is_end {res.push('.');}
+        return res;
+    }
+}
 
-// fn create_perms(periods_left: i64, spaces_left: i64, min: i64) -> Vec<Vec<i64>> {
-//     if spaces_left == 1 {
-//         return vec![vec![periods_left]];
-//     }
-//     let mut i: i64 = periods_left;
-//     let mut res: Vec<Vec<i64>> = Vec::new();
-//     while i >= min {
-//         for mut v in create_perms(periods_left-i, spaces_left-1, 1) {
-//             let mut temp: Vec<i64> = vec![i];
-//             temp.append(&mut v);
-//             res.push(temp);
-//         }
-//         i -= 1;
-//     }
-//     return res;
-// }
+fn apply_mask(values: &Vec<i64>, perm: &Vec<i64>) -> String {
+    let mut res: String = String::new();
+    res.push_str(&make_segment(*perm.first().unwrap(), true, false));
+
+    for i in 0..values.len()-1 {
+        res.push_str(&make_segment(values[i], false, false));
+        res.push_str(&make_segment(perm[i+1], true, false));
+    }
+    res.push_str(&make_segment(*values.last().unwrap(), false, true));
+    res.push_str(&make_segment(*perm.last().unwrap(), true, false));
+    return res;
+}
+
+fn create_perms(periods_left: i64, spaces_left: i64, min: i64) -> Vec<Vec<i64>> {
+    if spaces_left == 1 {
+        return vec![vec![periods_left]];
+    }
+    let mut i: i64 = periods_left;
+    let mut res: Vec<Vec<i64>> = Vec::new();
+    while i >= min {
+        for mut v in create_perms(periods_left-i, spaces_left-1, 0) {
+            let mut temp: Vec<i64> = vec![i];
+            temp.append(&mut v);
+            res.push(temp);
+        }
+        i -= 1;
+    }
+    return res;
+}
+
+fn get_perm_count(periods: i64, slots: i64) -> i64 {
+    return binomial_coefficient(periods+slots-1, slots-1);
+}
+
+fn binomial_coefficient(n: i64, m: i64) -> i64 {
+    let mut res = 1;
+ 
+    let mut m = m;
+    if (m > n - m) {
+        m = n - m;
+    }
+
+    let mut i = 0;
+    while i < m {
+        res *= (n - i);
+        res /= (i + 1);
+        i += 1;
+    }
+ 
+    return res;
+}
